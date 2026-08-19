06a8d1a19e3477980228afc278553b2d23975bfb
watson limprevil <limprevil35@gmail.com>
2026-08-19 18:15:01 -0400

    fixed a file path  issue

--- src/Api/client.js
+++ src/Api/client.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
 
 export async function api(path , options ={}){
     const token = localStorage.getItem('token');
